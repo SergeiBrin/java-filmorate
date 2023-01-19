95d0c92bc09f2e7ee648d2b047c5f6752287269d
Sergei <starodumovtmt@yandex.ru>
2023-01-19 16:00:13 +0300

    style: убрать лишний метод

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
@@ -1,16 +1,7 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-
 public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
         super(message);
-    }
-
-    // Отправляет клиенту код 400. Такое решение я нашел в гугле.
-    // Просто по дефолту в Postman приходит код 500, что, как мне кажется, не совсем правильно.
-    public void badRequest() {
-        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "(CODE 400)\n");
     }
 }
