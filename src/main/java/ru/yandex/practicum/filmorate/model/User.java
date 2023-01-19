8c2e211cb3284b7c9bc4e0465035fbfb31eeb94f
Sergei <starodumovtmt@yandex.ru>
2023-01-19 15:57:38 +0300

    feat: добавить аннотации для валидации

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,15 +3,23 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 public class User {
     private int id;
-    private String name;
+    private String name; // Может быть пустым
+    @NotBlank
+    @Pattern(regexp = "\\S+")
     private String login;
+    @PastOrPresent
     private LocalDate birthday;
+    @NotBlank
+    @Email
     private String email;
-
 }
