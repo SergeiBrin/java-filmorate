d0b0c2d539a5d3422347e0a54c6fbee5f4edb4de
Sergei <starodumovtmt@yandex.ru>
2023-02-05 04:38:21 +0500

    feat: добавить классы с логикой для userStorage и filmStorage

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
@@ -0,0 +1,45 @@
+package ru.yandex.practicum.filmorate.service.film;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class FilmService {
+    private final FilmStorage filmStorage;
+
+    @Autowired
+    public FilmService(InMemoryFilmStorage filmStorage) {
+        this.filmStorage = filmStorage;
+    }
+
+    public List<Film> getMostPopularFilm(Long count) {
+        return filmStorage.getPopularFilms()
+                .stream()
+                .limit(count)
+                .collect(Collectors.toList());
+    }
+
+    public Film likeTheFilm(Long filmId, Long userId) {
+        Film film = filmStorage.getFilmById(filmId);
+        film.addLike(userId);
+        filmStorage.updatePopularFilms(film);
+
+        return film;
+    }
+
+    public Film deleteLikeForFilm(Long filmId, Long userId) {
+        Film film = filmStorage.getFilmById(filmId);
+        film.deleteLike(userId);
+        filmStorage.updatePopularFilms(film);
+
+        return film;
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -0,0 +1,71 @@
+package ru.yandex.practicum.filmorate.service.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class UserService {
+    private final UserStorage userStorage;
+
+    @Autowired
+    public UserService(InMemoryUserStorage userStorage) { // Может быть тут нужен UserStorage?
+        this.userStorage = userStorage;
+    }
+
+    public List<User> getUserFriendsList(Long userId) {
+        List<User> friends = new ArrayList<>();
+        User user = userStorage.getUserById(userId);
+        Set<Long> friendId = user.getFriends();
+
+        for (Long id : friendId) {
+            friends.add(userStorage.getUserById(id));
+        }
+
+        return friends;
+    }
+
+    public List<User> getListOfMutualFriends(Long userId, Long friendId) {
+        List<User> friends = new ArrayList<>();
+
+        User user = userStorage.getUserById(userId);
+        User friend = userStorage.getUserById(friendId);
+
+        Set<Long> usersId = user.getFriends();
+        Set<Long> friendsId = friend.getFriends();
+
+        for (Long id : usersId) {
+            if (friendsId.contains(id)) {
+                friends.add(userStorage.getUserById(id));
+            }
+        }
+
+        return friends;
+    }
+
+    public User putFriendsToUser(Long userId, Long friendId) {
+        User user = userStorage.getUserById(userId);
+        User friend = userStorage.getUserById(friendId);
+
+        user.addFriend(friendId);
+        friend.addFriend(userId);
+
+        return userStorage.getUserById(userId);
+    }
+
+    public User deleteFriendFromUser(Long userId, Long friendId) {
+        User user = userStorage.getUserById(userId);
+        User friend = userStorage.getUserById(friendId);
+
+        user.deleteFriend(friendId);
+        friend.deleteFriend(userId);
+
+        return user;
+    }
+}
