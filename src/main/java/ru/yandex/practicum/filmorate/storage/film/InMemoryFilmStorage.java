86c5e3a6392e66360ef503408753416ca690551c
Sergei <starodumovtmt@yandex.ru>
2023-02-05 04:32:34 +0500

    feat: добавить хранилища для user и film

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -0,0 +1,81 @@
+package ru.yandex.practicum.filmorate.storage.film;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.*;
+
+@Component
+@Slf4j
+@Getter
+public class InMemoryFilmStorage implements FilmStorage {
+    private int id;
+    private final Map<Long, Film> films = new HashMap<>();
+    private final Set<Film> popularFilms = new TreeSet<>(createComparator());
+
+    // Сортировка наоборот, так как чем больше лайков, тем выше место в множестве.
+    private Comparator<Film> createComparator() {
+        return (o1, o2) -> {
+            // Это условие необходимо, чтобы в множество добавлялись разные фильмы
+            // c одинаковым количеством лайков.
+            if (o2.getLikes().size() - o1.getLikes().size() == 0) {
+                if (o2.getId() != o1.getId()) {
+                    return 1;
+                }
+            }
+            
+            return o2.getLikes().size() - o1.getLikes().size();
+        };
+    }
+
+    @Override
+    public List<Film> getAllFilms() {
+        log.info("Список всех фильмов {} отправлен клиенту", films.values());
+        return new ArrayList<>(films.values());
+    }
+
+    @Override
+    public Film getFilmById(Long filmId) {
+        log.info("Фильм с id {} отправлены клиенту", filmId);
+        return films.get(filmId);
+    }
+
+    @Override
+    public Film postFilm(Film film) {
+        film.setId(++id);
+
+        films.put(film.getId(), film);
+        log.info("Фильм {} добавлен в общий список", film);
+
+        if (popularFilms.add(film)) {
+            log.info("Фильм {} добавлен в популярный список", film);
+        }
+
+        return film;
+    }
+
+    @Override
+    public Film putFilm(Film film) {
+        films.put(film.getId(), film);
+        log.info("Фильм {} обновлён в общем списке", film);
+
+        updatePopularFilms(film);
+
+        return film;
+    }
+
+    @Override
+    public void updatePopularFilms(Film film) {
+        // Сначала удаляю фильм из множества, а потом добавляю его снова - для сортировки.
+        // Как добавить обновленный фильм в сортировку, с одновременным удалением его старой версии я понять не могу.
+        if (!popularFilms.isEmpty()) {
+            popularFilms.removeIf(checkFilm -> checkFilm.getId() == film.getId());
+        }
+
+        if (popularFilms.add(film)) {
+            log.info("Фильм {} обновлён в популярном списке", film);
+        }
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -0,0 +1,65 @@
+package ru.yandex.practicum.filmorate.storage.user;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@Getter
+@Setter
+@Slf4j
+public class InMemoryUserStorage implements UserStorage {
+    private int id;
+    private final Map<Long, User> users = new HashMap<>();
+
+    @Override
+    public List<User> getAllUsers() {
+        log.info("Список всех пользователей {} отправлен клиенту", users.values());
+        return new ArrayList<>(users.values());
+    }
+
+    @Override
+    public User getUserById(Long userId) {
+        log.info("Пользователь с id {} отправлены клиенту", userId);
+        return users.get(userId);
+    }
+
+
+    @Override
+    public User postUser(User user) {
+        checkName(user);
+        user.setId(++id);
+
+        users.put(user.getId(), user);
+        log.info("Пользователь добавлен {}", user);
+
+        return user;
+    }
+
+    @Override
+    public User putUser(User user) {
+        checkName(user);
+
+        // Если все проверки пройдены, то объект user обновляется.
+        users.put(user.getId(), user);
+        log.info("Пользователь обновлен {}", user);
+
+        return user;
+    }
+
+    // Если имя пустое, то login становится именем.
+    private void checkName(User user) {
+        boolean isNameCorrect = (user.getName() != null) && (!user.getName().isEmpty());
+
+        if (!isNameCorrect) {
+            user.setName(user.getLogin());
+        }
+    }
+}
